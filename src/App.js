c7fefea3d252f51909deae400e39e3b99c0ddd62
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2020-02-06 15:28:13 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Team from './Team/Team';
 import Header from './Header/Header';
 import Intro from './Intro/Intro';
 import Footer from './Footer/Footer';
-/* import FAQ from './Faq/Faq'; 
-import Speakers from './Speakers/Speakers'; */
-import Sponsors from './Sponsors/Sponsors';
+/* import FAQ from './Faq/Faq'; */
+import Speakers from './Speakers/Speakers'; 
+/* import Sponsors from './Sponsors/Sponsors'; */
 
 import './App.scss';
 
