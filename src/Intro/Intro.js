0159d2db82b2a514f69ebe76b4d3dff0e03faa19
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2020-02-06 15:22:15 +0400

    Update Intro.js

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -5,11 +5,11 @@ import './Intro.scss';
 const Intro = () => (
   <section className="Intro">
     <div className="IntroContainer">
-      <div className="Info">
+      /* <div className="Info">
         <p>Russian-Armenian University</p>
         <p>May 25, 2019</p>
-      </div>
-      <h1>React Conf Armenia</h1>
+      </div> */
+      <h1>Tech Events Armenia</h1>
     </div>
   </section>
 );
