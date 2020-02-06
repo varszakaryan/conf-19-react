fef2570ac4b4396922c2cb5b3725878a4cac30c5
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2020-02-06 15:41:40 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import useWindowScrollPosition from '@rehooks/window-scroll-position';
 
 import About from './About/About';
-/* import Location from './Location/Location'; */
 import Team from './Team/Team';
 import Header from './Header/Header';
 import Intro from './Intro/Intro';
 import Footer from './Footer/Footer';
-/* import FAQ from './Faq/Faq'; */
 import Speakers from './Speakers/Speakers'; 
-/* import Sponsors from './Sponsors/Sponsors'; */
 
 import './App.scss';
 
